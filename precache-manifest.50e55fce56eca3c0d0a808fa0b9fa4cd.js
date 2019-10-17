0351dd6c2cab6681f55a1f2aeedf43cbe1a64416
pherphepheni <osorio.f@outlook.es>
2019-10-17 19:40:40 -0300

    Updates

---
+++ precache-manifest.50e55fce56eca3c0d0a808fa0b9fa4cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7839e0f4bef7f7d30a8a7ac997baac",
+    "url": "/noticias/index.html"
+  },
+  {
+    "revision": "8fdab5477f0f5d448562",
+    "url": "/noticias/static/js/2.f62f9af2.chunk.js"
+  },
+  {
+    "revision": "768c62868b9bc8a79fac",
+    "url": "/noticias/static/js/main.2e79bdb1.chunk.js"
+  },
+  {
+    "revision": "56668b540b94fee4037f",
+    "url": "/noticias/static/js/runtime-main.2233eba8.js"
+  }
+]);
